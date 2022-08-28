4e8c1dc4f9cbac2756c68185e7b4946b14193a86
rohit20001221 <rohit20001221@gmail.com>
2022-08-28 22:59:38 +0530

    ..

--- frontend/algocrypt/App.tsx
+++
@@ -1,6 +0,0 @@
-import { NavigationProvider } from "./containers/NavigationContainer";
-import { routes } from "./navigation";
-
-export default function App() {
-  return <NavigationProvider routes={routes} />;
-}
